7f26e83e3df4bde43702c33cd2c5323d0ebbc611
Harsh Meshram <harsh.satishmeshram@students.iiserpune.ac.in>
2025-04-27 14:56:03 +0530

    Update interface.py

--- Harsh_Satish_Meshram/interface.py
+++ Harsh_Satish_Meshram/interface.py
@@ -1,6 +1,6 @@
 # interface.py
 from model import EfficientNetModel as TheModel   # If using CustomCNN, change it here
 from train import train_model as the_trainer
-from predict import predict_image as the_predictor
+from predict import predict_batch as the_predictor
 from dataset import WorkoutDataset as TheDataset
 from dataset import create_dataloader as the_dataloader
